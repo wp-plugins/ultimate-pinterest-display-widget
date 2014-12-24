11e3e24fdb939a6acea660b90816438752d74998
kris.lopac <kris.lopac@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-24 00:40:04 +0000

    change on ultimatePinterestDisplayWidget
    git-svn-id: https://plugins.svn.wordpress.org/ultimate-pinterest-display-widget/trunk@1053033 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ultimatePinterestDisplayWidget.php
+++ ultimatePinterestDisplayWidget.php
@@ -209,7 +209,7 @@ class ultimatePinterestDisplayWidget extends WP_Widget{
         $pinterestDisplay = "";
         $pinterestDisplay .= "<div id='ultimate-pinterest-display-widget' class='$suffix'>";
         $pinterestDisplay .= $outputDisplay;
-	$pinterestDisplay .= "<div class='copy' style='color:#ccc; font-size: 9px; '><a href='http://www.allpromotionalapparel.com' title='allpromotionalapparel.com' target='_blank'>promotional polo shirts</a></div>";
+		$pinterestDisplay .= "";
         $pinterestDisplay .= "</div>";
 		if($addpin == "true"){$pinterestDisplay .= "<script type='text/javascript' async src='//assets.pinterest.com/js/pinit.js'>";}
             echo $before_widget;
